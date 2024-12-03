68e42e8bf6ec763922894be9f1ba21c485733976
xoje3080 <jannes.ehrhardt@uni-oldenburg.de>
2024-12-03 10:29:43 +0100

    merge GER and FRA data - first steps

--- Code/Merge_Prep.R
+++ Code/Merge_Prep.R
@@ -43,14 +43,67 @@ df_GERs <- fread("../../Data/Tankerkoenig/stations/2022/preprocessed/stations_20
 
 
 
+# Data Inspection ---------------------------------------------------------
+
+str(df_FRA)
+
+str(df_GERp)
+
+str(df_GERs)
+
+# In df_FRA the id variable ist stored as int -> make it chr to be able to merge later
+
+df_FRA$station_id <- as.character(df_FRA$station_id)
+
+# Rename station_id to station_uuid to match the column name in df_GERp
+
+colnames(df_FRA)[colnames(df_FRA) == "station_id"] <- "station_uuid"
+
+# why so many observations in df_GERs?
+
+unique(df_GERs$uuid) |> length()
+
+
+# Merge Data -------------------------------------------------------------
+
+# Merge df_GERs (crossectional part) and df_GERp (time series part) on station_id
+
+# Set keys for both data.tables
+setkey(df_GERp, station_uuid, date_only)
+setkey(df_GERs, uuid, date)
+
+# Merge the two tables
+merged_GER <- merge(
+  df_GERp,
+  df_GERs,
+  by.x = c("station_uuid", "date_only"),
+  by.y = c("uuid", "date"),
+  all.x = TRUE,  # Keep all rows from df_GERp (time series data)
+  all.y = FALSE  # Only keep matching rows from df_GERs
+)
+
+
+str(merged_GER)
+
+str(df_FRA)
+
+
+# Add the GER dummy variable
+merged_GER[, dummy_GER := 1]
+df_FRA[, dummy_GER := 0]
+
+
+setnames(df_FRA, old = c("date", "E10", "Gazole"), 
+         new = c("date_only", "avg_e10", "avg_diesel"))
+
+
+# Bind the rows of the two data.tables
+df <- rbind(merged_GER, df_FRA, fill = TRUE)
 
 
 
-
-
-
-
-
+# how many stations in total?
+unique(df$station_uuid) |> length()
 
 
 
