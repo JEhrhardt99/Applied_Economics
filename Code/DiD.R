dac2e79c54d2128a71a8cd806ed8d5a8d4a82177
xoje3080 <jannes.ehrhardt@uni-oldenburg.de>
2024-12-11 15:07:20 +0100

    implement first dynamic approach - vector limit reached

--- Code/DiD.R
+++ Code/DiD.R
@@ -568,7 +568,74 @@ modelsummary(
 
 
 
+# DiD Dynamic ---------------------------------------------------
 
+
+#%%%%%%%% This is the textbook code example %%%%%%%%
+
+od <- causaldata::organ_donations
+
+# Treatment variable
+od <- od %>% mutate(California = State == 'California')
+
+# Interact quarter with being in the treated group using
+# the fixest i() function, which also lets us specify
+# a reference period (using the numeric version of Quarter)
+clfe <- feols(Rate ~ i(Quarter_Num, California, ref = 3) | 
+                State + Quarter_Num, data = od)
+
+# And use coefplot() for a graph of effects
+coefplot(clfe)
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+#%%%%%%%%%%%%%%%%%% Some Testing with function i() %%%%%%%%%%%%%%%%%%%%%%%%
+test1 <- feols(avg_diesel ~ dummy_GER:dummy_FTD |
+                 Station + date_only,
+               data = df_period)
+
+
+test2 <- feols(avg_diesel ~ i(dummy_GER, dummy_FTD, ref = 0) |
+                 Station + date_only,
+               data = df_period)
+
+
+test_list <- list()
+test_list[["test1"]] <- test1
+test_list[["test2"]] <- test2
+
+modelsummary(test_list,
+             stars = c('***' = 0.01, '**' = 0.05, '*' = 0.1),
+             gof_omit = "^(?!R2|Num)")
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+# Dynamic DiD Approach
+df_dynamic <- df_period 
+
+# Define the treatment date
+treatment_date <- as.IDate("2022-05-31")
+
+str(df_dynamic)
+str(treatment_date)
+
+# Create the centered_t variable
+df_dynamic[, centered_t := as.integer(date_only - treatment_date)]
+
+# Dynamic DiD Model
+dynamic_did <- feols(
+  avg_diesel ~ i(centered_t, dummy_GER, ref = 0) | Station + centered_t,
+  data = df_dynamic
+)
+
+# Summarize results
+summary(dynamic_did)
+
+# Plot the dynamic treatment effects
+coefplot(dynamic_did,
+         ci.width = "1%")
 
 
 
