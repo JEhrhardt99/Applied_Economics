6b4e2c94059093ac52464add279d4d8891851a39
xoje3080 <jannes.ehrhardt@uni-oldenburg.de>
2024-12-12 14:51:45 +0100

    save iplot tests (Dannemann) in experimental folder

--- Code/experimental/iplot_dynamic.R
+++ Code/experimental/iplot_dynamic.R
@@ -1,3 +1,12 @@
+
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) # set working directory to script location
+setwd("..") # move up to the project root directory
+getwd() # check if wd is the root directory
+
+library(fixest)
+library(data.table)
+
 df <- fread("../../Data/Preprocessed/final_data.csv.gz")[date_only >= "2022-04-01" & date_only <= "2022-08-31"]
 
 
@@ -23,6 +32,8 @@ dynamic_did <- feols(
     i(centered_t, dummy_GER, ref = 0):I(neighbors_count == 0) | station_uuid + centered_t,
   data = df
 )
+
+
 t1 <- Sys.time()
 print(t1 - t0)
 
